fefec1e1489257ecc7586cbe6424b23e9dadfc9a
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2021-12-22 00:32:20 +0100

    Create main.py
    Initial script creation

---
+++ StockTracker/main.py
@@ -0,0 +1,55 @@
+#!/user/bin/env python
+"""StockTracker Main.py"""
+
+#Import modules
+import json
+from datetime import date
+import requests
+import pandas as pd
+
+
+#modules
+def read_jsonfile(filename):
+    """Read data from file"""
+    with open(filename, encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    return data
+
+def get_api_key():
+    """Get API key from file"""
+    api_key = read_jsonfile('./.data/api/apikey.json')
+    return api_key['api_key']
+
+def get_daily_adjusted(symbol, api_key):
+    """Get daily adjusted data from API"""
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&outputsize=full&datatype=compact'
+    request = requests.get(url)
+    data = request.json()
+    return data
+
+def get_transactions():
+    """Get transactions from file"""
+    transactions = read_jsonfile('./.data/transactions/transactions.json')
+    return transactions
+
+def compute_transactions(transactions):
+    """Compute transactions"""
+    transactions = sorted(transactions, key=lambda k: k['date'])
+    end_date = date.today()
+    start_date = transactions[0]['date']
+    print(start_date, end_date)
+    daterange = pd.date_range(start_date, end_date)
+    for single_date in daterange:
+        print (single_date)
+
+#main
+def main():
+    """Main function"""
+    api_key = get_api_key()
+    symbol = 'MSFT'
+    data = get_daily_adjusted(symbol, api_key)
+    transactions = get_transactions()
+    compute_transactions(transactions['transactions'])
+
+if __name__ == '__main__':
+    main()
