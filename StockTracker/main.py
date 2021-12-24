4c2075d6ab0a79938e0ed1ecf893df26e86323b5
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2021-12-25 00:05:38 +0100

    calculate_sells_and_buys
    Made the calculate_sells_and_buys return one object so it is easier to analyze

--- StockTracker/main.py
+++ StockTracker/main.py
@@ -42,7 +42,7 @@ def compute_transactions(transactions):
     """Compute transactions"""
     transactions = sorted(transactions, key=lambda k: k['transaction_date'])
     stocks_held = get_transactions_by_day(transactions)
-    date_stocks_held_buys, date_stocks_held_sells = calculate_sells_and_buys(stocks_held) ## pylint: disable=unused-variable
+    stocks_held = calculate_sells_and_buys(stocks_held)
 
 def get_transactions_by_day(transactions):
     """Get transactions by day"""
@@ -77,14 +77,16 @@ def get_transactions_by_day(transactions):
 
 def calculate_sells_and_buys(stocks_held):
     """Merge sells and buys together"""
+
     #initialize variables
-    date_stocks_held_buys = []
-    date_stocks_held_sells = []
+    computed_date_stocks_held = []
 
     #loop through dates
     for date_stocks_held in stocks_held:
         #initialize variables
         symbols_buys = []
+        temp_list = []
+        symbols_sells = []
 
         #get buys
         filterd_date_stocks_held_buys = [d for d in date_stocks_held if d['transaction_type'] == "Buy"]
@@ -94,8 +96,7 @@ def calculate_sells_and_buys(stocks_held):
             symbols_buys.append(temp_loop['symbol'])
             symbols_buys = list(dict.fromkeys(symbols_buys))
 
-        #create buy object
-        temp_list = []
+        #create computed object
         for symbol_buys in symbols_buys:
             filterd_date_stock_held_buys = [d for d in filterd_date_stocks_held_buys if d['symbol'] == symbol_buys]
             temp_object = {
@@ -107,19 +108,15 @@ def calculate_sells_and_buys(stocks_held):
                 'date_held': filterd_date_stock_held_buys[0]['date_held']
             }
             temp_list.append(temp_object)
-        date_stocks_held_buys.append(temp_list)
 
-        #remove empty lists
-        date_stocks_held_buys = [x for x in date_stocks_held_buys if x]
 
         #repeat steps for sells
-        symbols_sells = []
+
         filterd_date_stocks_held_sells = [d for d in date_stocks_held if d['transaction_type'] == "Sell"]
         for temp_loop in filterd_date_stocks_held_sells:
             symbols_sells.append(temp_loop['symbol'])
             symbols_sells = list(dict.fromkeys(symbols_sells))
 
-        temp_list = []
         for symbol_sells in symbols_sells:
             filterd_date_stock_held_sells = [d for d in filterd_date_stocks_held_sells if d['symbol'] == symbol_sells]
             temp_object = {
@@ -131,13 +128,16 @@ def calculate_sells_and_buys(stocks_held):
                 'date_held': filterd_date_stock_held_sells[0]['date_held']
             }
             temp_list.append(temp_object)
-        date_stocks_held_sells.append(temp_list)
 
-        #remove empty lists
-        date_stocks_held_sells = [x for x in date_stocks_held_sells if x != []]
+        #append to list
+        computed_date_stocks_held.append(temp_list)
 
+    #remove empty lists
+    computed_date_stocks_held = [x for x in computed_date_stocks_held if x]
+    write_jsonfile(computed_date_stocks_held, './.data/output/computed_date_stocks_held.json')
     #return both lists
-    return date_stocks_held_buys, date_stocks_held_sells
+    return computed_date_stocks_held
+
 #main
 def main():
     """Main function"""
