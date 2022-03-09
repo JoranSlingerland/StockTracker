a941143abfdf7ae4d3ceee64d17d8bf91bed3805
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-03-09 23:45:08 +0100

    Added delete sql tables function

---
+++ delete_sql_tables/__init__.py
@@ -0,0 +1,46 @@
+"""Activity trigger"""
+
+import logging
+import pyodbc
+from shared_code import get_config
+
+
+def main(name: str) -> str:
+    """delete table"""
+    #pylint disable=unused-argument
+
+    logging.info("Deleting sql tables")
+
+    tables = get_config.get_tables()
+    sql_server = get_config.get_sqlserver()
+
+    # initialize variables
+    server = sql_server["sql_server"]["server"]
+    database = sql_server["sql_server"]["database"]
+    username = sql_server["sql_server"]["user"]
+    password = sql_server["sql_server"]["password"]
+    tables = tables["tables"]
+
+    # connect to database
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";DATABASE="
+        + database
+        + ";UID="
+        + username
+        + ";PWD="
+        + password
+    )
+    with conn:
+        crs = conn.cursor()
+        for table in tables:
+            if table["candelete"]:
+                crs.execute(
+                    f"""
+                drop table {table["table_name"]}
+                """
+                )
+
+
+    return "Success"
