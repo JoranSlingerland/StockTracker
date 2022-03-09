114fc4d200eb93fbdc4231bc1f79af6c0cff9399
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-03-10 00:08:58 +0100

    Truncate tables function

---
+++ truncate_sql_tables/__init__.py
@@ -0,0 +1,46 @@
+"""Activity trigger"""
+
+import logging
+import pyodbc
+from shared_code import get_config
+
+
+def main(name: str) -> str:
+    """truncate tables"""
+    # pylint: disable=unused-argument
+
+    tables = get_config.get_tables()
+    sql_server = get_config.get_sqlserver()
+
+    logging.info("Truncating sql tables")
+
+    # initialize variables
+    server = sql_server["sql_server"]["server"]
+    database = sql_server["sql_server"]["database"]
+    username = sql_server["sql_server"]["user"]
+    password = sql_server["sql_server"]["password"]
+    tables = tables["tables"]
+
+    # connect to database
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";DATABASE="
+        + database
+        + ";UID="
+        + username
+        + ";PWD="
+        + password
+    )
+
+    with conn:
+        crs = conn.cursor()
+        for table in tables:
+            if table["cantruncate"]:
+                crs.execute(
+                    f"""
+                truncate table {table["table_name"]}
+                """
+                )
+
+    return "Success"
