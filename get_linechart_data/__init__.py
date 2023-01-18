6c80bf968384e0e70fbc671cfab70e8e11ed9ffb
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-01-18 20:54:47 +0100

    Switch to chartjs

--- get_linechart_data/__init__.py
+++ get_linechart_data/__init__.py
@@ -5,6 +5,8 @@ import json
 from datetime import date, timedelta
 import azure.functions as func
 
+import pandas
+
 from shared_code import cosmosdb_module, date_time_helper
 
 
@@ -27,6 +29,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     container = cosmosdb_module.cosmosdb_container("totals")
     if datatoget == "max":
         items = list(container.read_all_items())
+        items = sorted(items, key=lambda k: k["date"])
+        start_date = items[0]["date"]
+        end_date = date.today().strftime("%Y-%m-%d")
     else:
         start_date, end_date = date_time_helper.datatogetswitch(datatoget)
         items = list(
@@ -39,48 +44,56 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 enable_cross_partition_query=True,
             )
         )
-    result = []
-    for item in items:
-        temp_list = inputoptions(datatype, item)
-        for temp_item in temp_list:
-            result.append(temp_item)
 
-    result = sorted(result, key=lambda k: k["date"])
+    result = new_result_object(datatype)
+    daterange = pandas.date_range(start_date, end_date)
 
+    for single_date in daterange:
+        single_date = single_date.strftime("%Y-%m-%d")
+        single_date_items = [x for x in items if x["date"] == single_date]
+
+        result["labels"].append(single_date)
+
+        for single_date_item in single_date_items:
+            if datatype == "invested_and_value":
+                result["datasets"][0]["data"].append(single_date_item["total_value"])
+                result["datasets"][1]["data"].append(single_date_item["total_invested"])
+            if datatype == "total_gains":
+                result["datasets"][0]["data"].append(single_date_item["total_pl"])
+
+    if not items:
+        return func.HttpResponse(
+            body='{"status": No data found in database for this timeframe"}',
+            mimetype="application/json",
+            status_code=500,
+        )
     if not result:
         return func.HttpResponse(
-            body='{"status": Please pass a valid name on the query string or in the request body"}',
+            body='{"status": Something went wrong"}',
             mimetype="application/json",
-            status_code=400,
+            status_code=500,
         )
     return func.HttpResponse(
         body=json.dumps(result), mimetype="application/json", status_code=200
     )
 
 
-def inputoptions(datatype, item):
-    """Home made match function"""
+def new_result_object(datatype):
+    """Generate result object"""
     if datatype == "invested_and_value":
-        return [
-            {
-                "date": item["date"],
-                "value": item["total_value"],
-                "category": "Value",
-            },
-            {
-                "date": item["date"],
-                "value": item["total_invested"],
-                "category": "Invested",
-            },
-        ]
+        return {
+            "labels": [],
+            "datasets": [
+                {"label": "Value", "borderColor": "#0e8505", "data": []},
+                {"label": "Invested", "borderColor": "#090a09", "data": []},
+            ],
+        }
     if datatype == "total_gains":
-        return [
-            {
-                "date": item["date"],
-                "value": item["total_pl"],
-                "category": "Gains",
-            }
-        ]
+        return {
+            "labels": [],
+            "datasets": [
+                {"label": "Gains", "borderColor": "#0e8505", "data": []},
+            ],
+        }
 
-    # return nothing if no match
     return None
