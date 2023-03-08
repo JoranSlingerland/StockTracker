48d4eebd5ca42c076da2c03415339c54fda954cc
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-03-08 21:30:37 +0100

    Fix orchestrator list + asyncio implemented

--- orchestartor_list/__init__.py
+++ orchestartor_list/__init__.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import json
 import azure.functions as func
 import azure.durable_functions as df
+from shared_code import aio_helper
 
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
@@ -15,28 +16,43 @@ async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
 
     output = []
     days = req.route_params["days"]
-    end_data = datetime.today()
-    start_date = end_data - timedelta(days=int(days))
+    end_date = datetime.today()
 
-    instances = await client.get_status_by(
-        created_time_from=start_date, created_time_to=end_data
-    )
+    tasks = []
+    for i in range(int(days)):
+        start_date = end_date - timedelta(days=i + 1)
+        end_date = end_date - timedelta(days=i)
+        tasks.append(get_orchestrations(start_date, end_date, client))
 
-    for instance in instances:
-        instance = instance.to_json()
-        if instance["name"] == "stocktracker_orchestrator":
-            output.append(instance)
+    output = await aio_helper.gather_with_concurrency(10, *tasks)
+    output = [item for sublist in output for item in sublist]
 
     output.sort(key=lambda x: x["createdTime"], reverse=True)
-
-    for instance in output:
-        instance["createdTime"] = instance["createdTime"].replace("T", " ")
-        instance["lastUpdatedTime"] = instance["lastUpdatedTime"].replace("T", " ")
-        instance["createdTime"] = instance["createdTime"].replace(".000000Z", "")
-        instance["lastUpdatedTime"] = instance["lastUpdatedTime"].replace(
-            ".000000Z", ""
-        )
 
     return func.HttpResponse(
         json.dumps(output), status_code=200, mimetype="application/json"
     )
+
+
+async def get_orchestrations(start_date, end_date, client):
+    """Get orchestrations"""
+    instances = await client.get_status_by(
+        created_time_from=start_date, created_time_to=end_date
+    )
+    output = []
+    for instance in instances:
+        instance = instance.to_json()
+        if instance["name"] == "stocktracker_orchestrator":
+            instance["createdTime"] = instance["createdTime"].replace("T", " ")
+            instance["lastUpdatedTime"] = instance["lastUpdatedTime"].replace("T", " ")
+            instance["createdTime"] = instance["createdTime"].replace(".000000Z", "")
+            instance["lastUpdatedTime"] = instance["lastUpdatedTime"].replace(
+                ".000000Z", ""
+            )
+            instance.pop("output", None)
+            instance.pop("input", None)
+            instance.pop("customStatus", None)
+            instance.pop("history", None)
+            instance.pop("name", None)
+            output.append(instance)
+    return output
