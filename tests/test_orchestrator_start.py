72fc6abbc6ade54cf8b63b5425cd44bb6ec91c68
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-03-14 22:58:46 +0100

    Switch to pytest

--- tests/test_orchestrator_start.py
+++ tests/test_orchestrator_start.py
@@ -1,142 +1,144 @@
 """Test Durable Functions Http Start."""
-# pylint: disable=import-error
 # pylint: disable=line-too-long
 
-from unittest import IsolatedAsyncioTestCase, mock
+from unittest import mock
 import azure.functions as func
 import azure.durable_functions as df
 from urllib3 import encode_multipart_formdata
-
+import pytest
 
 from orchestartor_start import main
 
+starter = mock.MagicMock()
 
-class DurableFunctionsHttpStartTestCase(IsolatedAsyncioTestCase):
+
+@pytest.mark.asyncio
+async def test_valid_data():
     """Test Durable Functions Http Start."""
+    body, header = encode_multipart_formdata(
+        {
+            "userId": "123",
+            "functionName": "stocktracker_orchestrator",
+            "daysToUpdate": "all",
+        }
+    )
+    header = {"Content-Type": header}
+    # starter = mock.MagicMock()
+    mock_request = func.HttpRequest(
+        method="POST",
+        body=body,
+        url="http://localhost:7071/api/orchestrator/start",
+        headers=header,
+    )
 
-    def setUp(self):
-        self.starter = mock.MagicMock()
+    mock_response = func.HttpResponse(
+        body=None, status_code=200, headers={"test": "test"}
+    )
 
-    async def test_valid_data(self):
-        """Test Durable Functions Http Start."""
-        body, header = encode_multipart_formdata(
-            {
-                "userId": "123",
-                "functionName": "stocktracker_orchestrator",
-                "daysToUpdate": "all",
-            }
-        )
-        header = {"Content-Type": header}
+    with mock.patch(
+        "azure.durable_functions.DurableOrchestrationClient",
+        spec=df.DurableOrchestrationClient,
+    ) as a_mock:
+        a_mock.start_new = mock.AsyncMock()
+        a_mock().start_new.return_value = "123"
+        a_mock().create_check_status_response.return_value = mock_response
+        response = await main(mock_request, starter)
+        assert response == mock_response
 
-        mock_request = func.HttpRequest(
-            method="POST",
-            body=body,
-            url="http://localhost:7071/api/orchestrator/start",
-            headers=header,
-        )
 
-        mock_response = func.HttpResponse(
-            body=None, status_code=200, headers={"test": "test"}
-        )
+@pytest.mark.asyncio
+async def test_invalid_userid():
+    """Test Durable Functions Http Start."""
+    body, header = encode_multipart_formdata(
+        {
+            "functionName": "stocktracker_orchestrator",
+            "daysToUpdate": "all",
+        }
+    )
+    header = {"Content-Type": header}
 
-        with mock.patch(
-            "azure.durable_functions.DurableOrchestrationClient",
-            spec=df.DurableOrchestrationClient,
-        ) as a_mock:
-            a_mock.start_new = mock.AsyncMock()
-            a_mock().start_new.return_value = "123"
-            a_mock().create_check_status_response.return_value = mock_response
-            response = await main(mock_request, self.starter)
-            self.assertEqual(response, mock_response)
+    mock_request = func.HttpRequest(
+        method="POST",
+        body=body,
+        url="http://localhost:7071/api/orchestrator/start",
+        headers=header,
+    )
 
-    async def test_invalid_userid(self):
-        """Test Durable Functions Http Start."""
-        body, header = encode_multipart_formdata(
-            {
-                "functionName": "stocktracker_orchestrator",
-                "daysToUpdate": "all",
-            }
-        )
-        header = {"Content-Type": header}
+    excpected_response = func.HttpResponse(
+        '{"status": "Please pass a valid user id in the request body"}',
+        status_code=400,
+    )
 
-        mock_request = func.HttpRequest(
-            method="POST",
-            body=body,
-            url="http://localhost:7071/api/orchestrator/start",
-            headers=header,
-        )
+    with mock.patch(
+        "azure.durable_functions.DurableOrchestrationClient",
+        spec=df.DurableOrchestrationClient,
+    ):
+        response = await main(mock_request, starter)
+        assert response.status_code == excpected_response.status_code
+        assert response.get_body() == excpected_response.get_body()
 
-        excpected_response = func.HttpResponse(
-            '{"status": "Please pass a valid user id in the request body"}',
-            status_code=400,
-        )
 
-        with mock.patch(
-            "azure.durable_functions.DurableOrchestrationClient",
-            spec=df.DurableOrchestrationClient,
-        ):
-            response = await main(mock_request, self.starter)
-            self.assertEqual(response.status_code, excpected_response.status_code)
-            self.assertEqual(response.get_body(), excpected_response.get_body())
+@pytest.mark.asyncio
+async def test_invalid_function_name():
+    """check invalid function name"""
+    body, header = encode_multipart_formdata(
+        {
+            "userId": "123",
+            "functionName": "stocktracker_orchestrator1",
+            "daysToUpdate": "all",
+        }
+    )
+    header = {"Content-Type": header}
 
-    async def test_invalid_function_name(self):
-        """check invalid function name"""
-        body, header = encode_multipart_formdata(
-            {
-                "userId": "123",
-                "functionName": "stocktracker_orchestrator1",
-                "daysToUpdate": "all",
-            }
-        )
-        header = {"Content-Type": header}
+    mock_request = func.HttpRequest(
+        method="POST",
+        body=body,
+        url="http://localhost:7071/api/orchestrator/start",
+        headers=header,
+    )
 
-        mock_request = func.HttpRequest(
-            method="POST",
-            body=body,
-            url="http://localhost:7071/api/orchestrator/start",
-            headers=header,
-        )
+    excpected_response = func.HttpResponse(
+        '{"status": "Please pass a valid function name in the route parameters"}',
+        status_code=400,
+    )
 
-        excpected_response = func.HttpResponse(
-            '{"status": "Please pass a valid function name in the route parameters"}',
-            status_code=400,
-        )
+    with mock.patch(
+        "azure.durable_functions.DurableOrchestrationClient",
+        spec=df.DurableOrchestrationClient,
+    ):
+        response = await main(mock_request, starter)
+        assert response.status_code == excpected_response.status_code
+        assert response.get_body() == excpected_response.get_body()
 
-        with mock.patch(
-            "azure.durable_functions.DurableOrchestrationClient",
-            spec=df.DurableOrchestrationClient,
-        ):
-            response = await main(mock_request, self.starter)
-            self.assertEqual(response.status_code, excpected_response.status_code)
-            self.assertEqual(response.get_body(), excpected_response.get_body())
 
-    async def test_invalid_days_to_update(self):
-        """check invalid days to update"""
-        body, header = encode_multipart_formdata(
-            {
-                "userId": "123",
-                "functionName": "stocktracker_orchestrator",
-                "daysToUpdate": "all1",
-            }
-        )
-        header = {"Content-Type": header}
+@pytest.mark.asyncio
+async def test_invalid_days_to_update():
+    """check invalid days to update"""
+    body, header = encode_multipart_formdata(
+        {
+            "userId": "123",
+            "functionName": "stocktracker_orchestrator",
+            "daysToUpdate": "all1",
+        }
+    )
+    header = {"Content-Type": header}
 
-        mock_request = func.HttpRequest(
-            method="POST",
-            body=body,
-            url="http://localhost:7071/api/orchestrator/start",
-            headers=header,
-        )
+    mock_request = func.HttpRequest(
+        method="POST",
+        body=body,
+        url="http://localhost:7071/api/orchestrator/start",
+        headers=header,
+    )
 
-        excpected_response = func.HttpResponse(
-            '{"status": "Please pass a valid number of days to update or pass all in the route parameters"}',
-            status_code=400,
-        )
+    excpected_response = func.HttpResponse(
+        '{"status": "Please pass a valid number of days to update or pass all in the route parameters"}',
+        status_code=400,
+    )
 
-        with mock.patch(
-            "azure.durable_functions.DurableOrchestrationClient",
-            spec=df.DurableOrchestrationClient,
-        ):
-            response = await main(mock_request, self.starter)
-            self.assertEqual(response.status_code, excpected_response.status_code)
-            self.assertEqual(response.get_body(), excpected_response.get_body())
+    with mock.patch(
+        "azure.durable_functions.DurableOrchestrationClient",
+        spec=df.DurableOrchestrationClient,
+    ):
+        response = await main(mock_request, starter)
+        assert response.status_code == excpected_response.status_code
+        assert response.get_body() == excpected_response.get_body()
--- tests/test_shared_code.py
+++ tests/test_shared_code.py
@@ -1,12 +1,11 @@
 """Test get_config.py"""
-# pylint: disable=import-error
 
-import unittest
 from unittest import mock
 import asyncio
 import datetime
 import os
 from azure.cosmos import errors
+import pytest
 
 from shared_code import (
     aio_helper,
@@ -18,22 +17,20 @@ from shared_code import (
 )
 
 
-class TestAioHelper(unittest.TestCase):
-    """Test aio_helper.py"""
+@pytest.mark.asyncio
+async def test_gather_with_concurrency():
+    """Test gather with concurrency"""
 
-    async def test_gather_with_concurrency(self):
-        """Test gather with concurrency"""
+    async def my_coroutine(test_input):
+        await asyncio.sleep(0.1)
+        return test_input * 2
 
-        async def my_coroutine(test_input):
-            await asyncio.sleep(0.1)
-            return test_input * 2
-
-        tasks = [my_coroutine(i) for i in range(10)]
-        result = await aio_helper.gather_with_concurrency(5, *tasks)
-        self.assertEqual(result, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+    tasks = [my_coroutine(i) for i in range(10)]
+    result = await aio_helper.gather_with_concurrency(5, *tasks)
+    assert result == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 
 
-class TestCosmosdbModule(unittest.TestCase):
+class TestCosmosdbModule():
     """Test cosmosdb module"""
 
     @mock.patch("shared_code.get_config.get_cosmosdb")
@@ -53,7 +50,7 @@ class TestCosmosdbModule(unittest.TestCase):
         client = cosmosdb_module.cosmosdb_client()
 
         mock_cosmos_client.assert_called_once_with("mock_endpoint", "mock_key")
-        self.assertEqual(client, mock_client)
+        assert client == mock_client
 
     @mock.patch("shared_code.cosmosdb_module.cosmosdb_client")
     @mock.patch("shared_code.cosmosdb_module.get_config.get_cosmosdb")
@@ -67,7 +64,7 @@ class TestCosmosdbModule(unittest.TestCase):
         result = cosmosdb_module.cosmosdb_database()
 
         # Assert that the result is as expected
-        self.assertEqual(result, mock_database_client)
+        assert result == mock_database_client
 
     @mock.patch("shared_code.cosmosdb_module.cosmosdb_database")
     def test_cosmosdb_container(self, mock_cosmosdb_database):
@@ -76,7 +73,7 @@ class TestCosmosdbModule(unittest.TestCase):
         mock_database = mock_cosmosdb_database.return_value
         mock_container_client = mock_database.get_container_client.return_value
         result = cosmosdb_module.cosmosdb_container("mock container name")
-        self.assertEqual(result, mock_container_client)
+        assert result == mock_container_client
 
     async def test_container_function_with_back_off(self):
         """Test container function with back off"""
@@ -89,7 +86,7 @@ class TestCosmosdbModule(unittest.TestCase):
         try:
             await cosmosdb_module.container_function_with_back_off(function)
         except Exception as err:
-            self.assertIsInstance(err, errors.CosmosResourceExistsError)
+            assert isinstance(err, errors.CosmosResourceExistsError)
         function.assert_called_once()
 
         function.reset_mock()
@@ -97,7 +94,7 @@ class TestCosmosdbModule(unittest.TestCase):
         try:
             await cosmosdb_module.container_function_with_back_off(function)
         except Exception as err:
-            self.assertIsInstance(err, errors.CosmosHttpResponseError)
+            assert isinstance(err, errors.CosmosHttpResponseError)
         function.assert_called_once()
 
         function.reset_mock()
@@ -105,11 +102,11 @@ class TestCosmosdbModule(unittest.TestCase):
         try:
             await cosmosdb_module.container_function_with_back_off(function)
         except Exception as err:
-            self.assertIsInstance(err, Exception)
-            self.assertEqual(function.call_count, 10)
+            assert isinstance(err, Exception)
+            assert function.call_count == 10
 
 
-class TestGetConfig(unittest.TestCase):
+class TestGetConfig:
     """Test get_config.py"""
 
     @mock.patch.dict(os.environ, {"API_KEY": "test_api_key"})
@@ -117,14 +114,14 @@ class TestGetConfig(unittest.TestCase):
         """Test get api key"""
 
         api_key = get_config.get_api_key()
-        self.assertEqual(api_key, "test_api_key")
+        assert api_key == "test_api_key"
 
     @mock.patch.dict(os.environ, {"CLEARBIT_API_KEY": "test_clearbit_api_key"})
     def test_get_clearbit_api_key(self):
         """Test get clearbit api key"""
 
         clearbit_api_key = get_config.get_clearbit_api_key()
-        self.assertEqual(clearbit_api_key, "test_clearbit_api_key")
+        assert clearbit_api_key == "test_clearbit_api_key"
 
     @mock.patch.dict(
         os.environ,
@@ -139,41 +136,41 @@ class TestGetConfig(unittest.TestCase):
         """Test get cosmosdb"""
 
         cosmosdb = get_config.get_cosmosdb()
-        self.assertEqual(cosmosdb["endpoint"], "test_endpoint")
-        self.assertEqual(cosmosdb["key"], "test_key")
-        self.assertEqual(cosmosdb["database"], "test_database")
-        self.assertEqual(cosmosdb["offer_throughput"], "test_offer_throughput")
+        assert cosmosdb["endpoint"] == "test_endpoint"
+        assert cosmosdb["key"] == "test_key"
+        assert cosmosdb["database"] == "test_database"
+        assert cosmosdb["offer_throughput"] == "test_offer_throughput"
 
 
-class TestDateTimeHelper(unittest.TestCase):
+class TestDateTimeHelper:
     """Test date time helper"""
 
     def test_get_quarter_first_and_last_date(self):
         """Test get quarter first and last date"""
         quarter = "Q1 2021"
         start_date, end_date = date_time_helper.get_quarter_first_and_last_date(quarter)
-        self.assertEqual(start_date, datetime.date(2021, 1, 1))
-        self.assertEqual(end_date, datetime.date(2021, 3, 31))
+        assert start_date == datetime.date(2021, 1, 1)
+        assert end_date == datetime.date(2021, 3, 31)
 
         quarter = "Q2 2021"
         start_date, end_date = date_time_helper.get_quarter_first_and_last_date(quarter)
-        self.assertEqual(start_date, datetime.date(2021, 4, 1))
-        self.assertEqual(end_date, datetime.date(2021, 6, 30))
+        assert start_date == datetime.date(2021, 4, 1)
+        assert end_date == datetime.date(2021, 6, 30)
 
         quarter = "Q3 2021"
         start_date, end_date = date_time_helper.get_quarter_first_and_last_date(quarter)
-        self.assertEqual(start_date, datetime.date(2021, 7, 1))
-        self.assertEqual(end_date, datetime.date(2021, 9, 30))
+        assert start_date == datetime.date(2021, 7, 1)
+        assert end_date == datetime.date(2021, 9, 30)
 
         quarter = "Q4 2021"
         start_date, end_date = date_time_helper.get_quarter_first_and_last_date(quarter)
-        self.assertEqual(start_date, datetime.date(2021, 10, 1))
-        self.assertEqual(end_date, datetime.date(2021, 12, 31))
+        assert start_date == datetime.date(2021, 10, 1)
+        assert end_date == datetime.date(2021, 12, 31)
 
         quarter = "Q5 2021"
         start_date, end_date = date_time_helper.get_quarter_first_and_last_date(quarter)
-        self.assertEqual(start_date, None)
-        self.assertEqual(end_date, None)
+        assert None is start_date
+        assert None is end_date
 
     def test_datatogetswitch(self):
         """Test data to get switch"""
@@ -182,114 +179,108 @@ class TestDateTimeHelper(unittest.TestCase):
         """Test month to quarter"""
         month = "January"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q1")
+        assert quarter == "Q1"
 
         month = "February"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q1")
+        assert quarter == "Q1"
 
         month = "March"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q1")
+        assert quarter == "Q1"
 
         month = "April"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q2")
+        assert quarter == "Q2"
 
         month = "May"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q2")
+        assert quarter == "Q2"
 
         month = "June"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q2")
+        assert quarter == "Q2"
 
         month = "July"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q3")
+        assert quarter == "Q3"
 
         month = "August"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q3")
+        assert quarter == "Q3"
 
         month = "September"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q3")
+        assert quarter == "Q3"
 
         month = "October"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q4")
+        assert quarter == "Q4"
 
         month = "November"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q4")
+        assert quarter == "Q4"
 
         month = "December"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, "Q4")
+        assert quarter == "Q4"
 
         month = "Not a month"
         quarter = date_time_helper.month_to_quarter(month)
-        self.assertEqual(quarter, None)
+        assert quarter is None
 
     def test_get_quarter(self):
         """test get quarter"""
         start_date = datetime.date(2021, 1, 1)
         end_date = datetime.date(2021, 3, 31)
         quarters = date_time_helper.get_quarters(start_date, end_date)
-        self.assertEqual(quarters, ["Q1 2021"])
+        assert quarters == ["Q1 2021"]
 
         start_date = datetime.date(2021, 1, 1)
         end_date = datetime.date(2022, 3, 31)
         quarters = date_time_helper.get_quarters(start_date, end_date)
-        self.assertEqual(
-            quarters, ["Q1 2021", "Q2 2021", "Q3 2021", "Q4 2021", "Q1 2022"]
-        )
+        assert quarters == ["Q1 2021", "Q2 2021", "Q3 2021", "Q4 2021", "Q1 2022"]
 
         start_date = datetime.date(2021, 1, 1)
         end_date = datetime.date(2020, 12, 31)
         quarters = date_time_helper.get_quarters(start_date, end_date)
-        self.assertEqual(quarters, [])
+        assert not quarters
 
     def test_get_months(self):
         """test get months"""
         start_date = datetime.date(2021, 1, 1)
         end_date = datetime.date(2021, 3, 31)
         months = date_time_helper.get_months(start_date, end_date)
-        self.assertEqual(
-            months,
-            [
-                datetime.date(2021, 1, 31),
-                datetime.date(2021, 2, 28),
-                datetime.date(2021, 3, 31),
-            ],
-        )
+        assert months == [
+            datetime.date(2021, 1, 31),
+            datetime.date(2021, 2, 28),
+            datetime.date(2021, 3, 31),
+        ]
 
         start_date = datetime.date(2021, 1, 1)
         end_date = datetime.date(2020, 12, 31)
         months = date_time_helper.get_months(start_date, end_date)
-        self.assertEqual(months, [])
+        assert months == []
 
     def test_get_weeks(self):
         """test get weeks"""
 
 
-class TestSchemas(unittest.TestCase):
-    """ "Test schemas"""
+class TestSchemas:
+    """Test schemas"""
 
     def test_stock_input(self):
         """Test stock input"""
-
         schema = schemas.stock_input()
-        self.assertIsInstance(schema, dict)
+        assert isinstance(schema, dict)
 
     def test_transaction_input(self):
         """Test stock output"""
         schema = schemas.transaction_input()
-        self.assertIsInstance(schema, dict)
+        assert isinstance(schema, dict)
 
 
-class TestUtils(unittest.TestCase):
+class TestUtils:
     """Test utils"""
 
     def test_get_unique_items(self):
@@ -304,19 +295,18 @@ class TestUtils(unittest.TestCase):
         ]
         key_to_filter = "key_to_filter"
         unique_items = utils.get_unique_items(items, key_to_filter)
-        self.assertEqual(unique_items, ["a", "b", "c"])
+        assert unique_items == ["a", "b", "c"]
 
         items = []
-
         unique_items = utils.get_unique_items(items, key_to_filter)
-        self.assertEqual(unique_items, [])
+        assert not unique_items
 
     def test_get_weighted_average(self):
         """Test get weighted average"""
         data = [1, 4]
         weight = [1, 2]
         weighted_average = utils.get_weighted_average(data, weight)
-        self.assertEqual(weighted_average, 3.0)
+        assert weighted_average == 3.0
 
     def test_add_meta_data_to_stock_data2(self):
         """Test add meta data to stock data"""
@@ -329,21 +319,15 @@ class TestUtils(unittest.TestCase):
         container = mock.Mock()
         container.read_all_items.return_value = meta_data
         result = utils.add_meta_data_to_stock_data(stock_data, container)
-        self.assertEqual(
-            result,
-            [
-                {"symbol": "ABC", "meta": {"symbol": "ABC", "name": "ABC Inc."}},
-                {"symbol": "XYZ", "meta": {"symbol": "XYZ", "name": "XYZ Inc."}},
-            ],
-        )
+        assert result == [
+            {"symbol": "ABC", "meta": {"symbol": "ABC", "name": "ABC Inc."}},
+            {"symbol": "XYZ", "meta": {"symbol": "XYZ", "name": "XYZ Inc."}},
+        ]
 
         # Test when no meta data is found
         container.read_all_items.return_value = []
         result = utils.add_meta_data_to_stock_data(stock_data, container)
-        self.assertEqual(
-            result,
-            [
-                {"symbol": "ABC", "meta": {}},
-                {"symbol": "XYZ", "meta": {}},
-            ],
-        )
+        assert result == [
+            {"symbol": "ABC", "meta": {}},
+            {"symbol": "XYZ", "meta": {}},
+        ]
