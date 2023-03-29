e2519727c02035537fd954c9b31dfcea520ebb6c
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-03-29 18:56:45 +0200

    #51 + #207 Add unit testing

---
+++ tests/test_delete_cosmosdb_items.py
@@ -0,0 +1,52 @@
+"""Test the delete_cosmosdb_items function."""
+
+from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
+from azure.cosmos import ContainerProxy
+
+from delete_cosmosdb_items import main
+
+mock_items = [
+    {
+        "id": "1",
+    }
+]
+
+
+@pytest.mark.asyncio()
+@patch("shared_code.cosmosdb_module.cosmosdb_container")
+async def test_all(cosmosdb_container_mock):
+    """Test the main function."""
+    payload = ["all", "123"]
+
+    cosmosdb_container_mock.return_value = MagicMock(spec=ContainerProxy)
+    cosmosdb_container_mock.return_value.query_items.return_value = mock_items
+    cosmosdb_container_mock.return_value.delete_item = AsyncMock()
+
+    response = await main(payload)
+
+    assert response == '{"status": "Done"}'
+    assert cosmosdb_container_mock.return_value.delete_item.await_count == 4
+    cosmosdb_container_mock.return_value.delete_item.assert_called_with(
+        mock_items[0], partition_key=mock_items[0]["id"]
+    )
+
+
+@pytest.mark.asyncio()
+@patch("shared_code.cosmosdb_module.cosmosdb_container")
+async def test_days(cosmosdb_container_mock):
+    """Test the main function."""
+    payload = [1, "123"]
+
+    cosmosdb_container_mock.return_value = MagicMock(spec=ContainerProxy)
+    cosmosdb_container_mock.return_value.query_items.return_value = mock_items
+    cosmosdb_container_mock.return_value.delete_item = AsyncMock()
+
+    response = await main(payload)
+
+    assert response == '{"status": "Done"}'
+    assert cosmosdb_container_mock.return_value.delete_item.await_count == 4
+    cosmosdb_container_mock.return_value.delete_item.assert_called_with(
+        mock_items[0], partition_key=mock_items[0]["id"]
+    )
