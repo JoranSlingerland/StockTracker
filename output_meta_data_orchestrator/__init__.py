3278c76f5e7bbcddfe1d3964af79b5c3b2b04e96
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-05-11 21:13:52 +0200

    #264

--- output_meta_data_orchestrator/__init__.py
+++ output_meta_data_orchestrator/__init__.py
@@ -42,7 +42,11 @@ def orchestrator_function(context: df.DurableOrchestrationContext):
     )
 
     data = get_stock_meta_data(
-        symbols_to_update, transactions, user_data["clearbit_api_key"], userid
+        symbols_to_update,
+        transactions,
+        user_data["clearbit_api_key"],
+        userid,
+        user_data["brandfetch_api_key"],
     )
 
     result = yield context.call_activity("output_to_cosmosdb", ["meta_data", data])
@@ -61,8 +65,26 @@ def call_clearbit_api(url: str, clearbit_api_key: str) -> dict:
     return None
 
 
+def call_brandfetch_api(url: str, brandfetch_api_key: str) -> dict:
+    """Call the brandfetch API"""
+    logging.info(f"Calling Brandfetch API: {url}")
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {brandfetch_api_key}",
+    }
+    response = requests.get(url, headers=headers, timeout=10)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
 def get_stock_meta_data(
-    symbols: list, transactions: list, clearbit_api_key: str, userid: str
+    symbols: list,
+    transactions: list,
+    clearbit_api_key: str,
+    userid: str,
+    brandfetch_api_key: str,
 ) -> list:
     """Get stock meta data from the API"""
     logging.info("Getting stock meta data")
@@ -74,23 +96,72 @@ def get_stock_meta_data(
         domain = [x for x in transactions if x["symbol"] == symbol]
         domain = domain[0]["domain"]
 
-        url = f"https://company.clearbit.com/v2/companies/find?domain={domain}"
-        temp_data = call_clearbit_api(url, clearbit_api_key)
+        clearbit_data = call_clearbit_api(
+            f"https://company.clearbit.com/v2/companies/find?domain={domain}",
+            clearbit_api_key,
+        )
+        brandfetch_data = call_brandfetch_api(
+            f"https://api.brandfetch.io/v2/brands/{domain}", brandfetch_api_key
+        )
+
+        logos = filtered_logos(brandfetch_data.get("logos", []))
+        images = filtered_images(brandfetch_data.get("images", []))
 
         temp_object = {
             "symbol": symbol,
-            "name": temp_data["name"],
-            "description": temp_data["description"],
-            "country": temp_data["geo"]["country"],
-            "sector": temp_data["category"]["sector"],
+            "name": clearbit_data.get("name", None),
+            "description": clearbit_data.get("description", None),
+            "country": clearbit_data.get("geo", {}).get("country", None),
+            "sector": clearbit_data.get("category", {}).get("sector", None),
             "domain": domain,
-            "logo": f"https://logo.uplead.com/{domain}",
+            "links": brandfetch_data.get("links", None),
             "id": str(uuid.uuid4()),
             "expiry": (date.today() + timedelta(days=30)).strftime("%Y-%m-%d"),
             "userid": userid,
         }
+
+        temp_object.update(logos)
+        temp_object.update(images)
+
         output.append(temp_object)
     return output
 
 
 main = df.Orchestrator.create(orchestrator_function)
+
+
+def filtered_logos(logos):
+    """Filter logos"""
+
+    result = {}
+    preferred_theme = "light"
+
+    for type in ["logo", "symbol", "icon"]:
+        filtered_logos = [x for x in logos if x["type"] == type]
+        if not filtered_logos:
+            result[type] = None
+            continue
+        filtered_themes = [x for x in filtered_logos if x["theme"] == preferred_theme]
+
+        if not filtered_themes:
+            result[type] = filtered_logos[0]["formats"][0]["src"]
+            continue
+
+        result[type] = filtered_themes[0]["formats"][0]["src"]
+
+    result["symbol_img"] = result.pop("symbol")
+
+    return result
+
+
+def filtered_images(images):
+    """Filter images"""
+
+    result = {}
+
+    filtered_images = [x for x in images if x["type"] == "banner"]
+    if not filtered_images:
+        result["banner"] = None
+    result["banner"] = filtered_images[0]["formats"][0]["src"]
+
+    return result
