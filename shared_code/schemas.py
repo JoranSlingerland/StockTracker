4d0b19d2430a740f253a5d06dcb138b14285f0ce
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2023-05-18 15:26:11 +0200

    Support system dark mode

--- shared_code/schemas.py
+++ shared_code/schemas.py
@@ -84,12 +84,13 @@ def user_data() -> dict:
     return {
         "type": "object",
         "properties": {
-            "dark_mode": {"type": "boolean"},
+            "dark_mode": {"type": "string", "enum": ["dark", "light", "system"]},
             "clearbit_api_key": {"type": "string", "minLength": 1},
             "alpha_vantage_api_key": {"type": "string", "minLength": 1},
             "brandfetch_api_key": {"type": "string", "minLength": 1},
             "currency": {"type": "string", "minLength": 1, "maxLength": 3},
             "isLoading": {"type": "boolean"},
+            "isError": {"type": "boolean"},
         },
         "additionalProperties": False,
         "required": [
@@ -99,5 +100,6 @@ def user_data() -> dict:
             "brandfetch_api_key",
             "currency",
             "isLoading",
+            "isError",
         ],
     }
